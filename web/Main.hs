e38e5df16fc971bb913b3c4c861176b47350bab7
Ian Henry <ianthehenry@gmail.com>
2015-08-23 22:12:48 -0400

    web: signup works now
    *Sort of*. It doesn't send the confirmation email that it should,
    but it does create the user and token rows.

--- web/Main.hs
+++ web/Main.hs
@@ -2,8 +2,9 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RankNTypes #-}
 
-import BasePrelude
+import BasePrelude hiding (left)
 import Control.Monad.IO.Class
+import Control.Monad.Trans
 import Control.Monad.Trans.Either
 import qualified Data.Configurator as Conf
 import Dismissive.Api
@@ -15,6 +16,8 @@ import Servant
 import Servant.HTML.Lucid
 import Lucid
 import Landing
+import Dismissive.Types
+import Dismissive.Api
 
 data Login = Login Text Text
 
@@ -46,10 +49,28 @@ layout (stylesheets, scripts, inner) = html_ (head_ head <> body_ inner)
 simplePage :: HtmlT m () -> Partial m
 simplePage = ([], [], )
 
+emailConfirm :: Monad m => EmailAddress -> Partial m
+emailConfirm email = simplePage $ do
+  h1_ "Welcome to Dismissive"
+  (p_ . toHtml) ("An email is on its way to " <> email <> " with all the details.")
+  a_ [href_ "https://en.wikipedia.org/wiki/Special:Random"] "Check out a random Wikipedia article while you wait."
+
+sendInitialEmail :: MonadIO m => EmailAddress -> Token -> m ()
+sendInitialEmail email token = liftIO $ do
+  print email
+  print token
+  print "done"
+
 dynamicServer :: ServerT DynamicApi (DismissiveT (EitherT ServantErr IO))
 dynamicServer = handleLandingPage :<|> handleAuth
   where
-    handleAuth (Login username email) = return (layout $ simplePage $ div_ $ toHtml $ "okay!" <> email)
+    handleAuth (Login "" email) = do
+      createAccount email >>= \case
+        Left EmailAlreadyExists -> return () -- maybe send an email here too, baby
+        Left TokenNonsense -> lift (left err500)
+        Right token -> sendInitialEmail email token -- send an email, baby
+      return $ layout (emailConfirm email)
+    handleAuth _ = (return . layout . simplePage . div_) "Alright! Account successfully created."
     handleLandingPage = return (layout signupPage)
 
 server :: DismissiveIO (Server Api)
