d7322af7f9c9dd597437753e6690b5d0d57536ac
Ian Henry <ianthehenry@gmail.com>
2015-08-27 22:02:15 -0400

    servant: update for the new DismissiveT

--- servant/Dismissive/Servant.hs
+++ servant/Dismissive/Servant.hs
@@ -3,20 +3,12 @@
 module Dismissive.Servant where
 
 import BasePrelude
-import Control.Monad.State
-import Control.Monad.Reader
 import Control.Monad.Trans.Either
+import Control.Monad.Logger
 import Dismissive.Api
 import Servant
-import Network.Wai
 
-type Dismiss = DismissiveT (EitherT ServantErr IO) :~> EitherT ServantErr IO
+type Dismiss = DismissiveT (LoggingT (EitherT ServantErr IO)) :~> EitherT ServantErr IO
 
-getDismiss :: Monad m => DismissiveT m Dismiss
-getDismiss = do
-  pool <- ask
-  rng <- get
-  return $ Nat (runDismissiveT rng pool)
-
-makeApplication :: (MonadIO m, HasServer a) => Proxy a -> DismissiveT m (Server a) -> DismissiveT m Application
-makeApplication api d = serve api <$> d
+getDismiss :: ConnectionString -> Dismiss
+getDismiss connStr = Nat (runStderrLoggingT . execDismissive connStr)
