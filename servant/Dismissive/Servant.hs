2c0b6445afb91548991c596f432f84e9e9d664d7
Ian Henry <ianthehenry@gmail.com>
2015-08-30 13:59:14 -0400

    servant: support the new withDismissiveIO

--- servant/Dismissive/Servant.hs
+++ servant/Dismissive/Servant.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Dismissive.Servant where
 
 import BasePrelude
 import Control.Monad.Trans.Either
+import Control.Monad.IO.Class
 import Control.Monad.Logger
+import Control.Monad.Trans.Control
 import Dismissive.Api
 import Servant
 
-type Dismiss = DismissiveT (LoggingT (EitherT ServantErr IO)) :~> EitherT ServantErr IO
+type Dismiss = DismissiveT (EitherT ServantErr IO) :~> EitherT ServantErr IO
 
-getDismiss :: ConnectionString -> Dismiss
-getDismiss connStr = Nat (runStderrLoggingT . execDismissive connStr)
+getDismiss :: (MonadIO m, MonadBaseControl IO m, MonadBaseControl IO m', MonadLogger m', MonadIO m')
+           => ConnectionString
+           -> ((DismissiveT m :~> m) -> m' a)
+           -> m' a
+getDismiss connStr f =
+  withDismissiveIO
+  connStr
+  -- GHC can't typecheck the pointfree equivalent (f . Nat), and I don't know why
+  (\r -> f $ Nat r)
