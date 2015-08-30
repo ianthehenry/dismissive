b19d3e37598c3a7abc4d044dc8a1fe6d14d7acd7
Ian Henry <ianthehenry@gmail.com>
2015-08-30 18:50:49 -0400

    servant: expect MailerT in the stack

--- servant/Dismissive/Servant.hs
+++ servant/Dismissive/Servant.hs
@@ -9,16 +9,16 @@ import Control.Monad.IO.Class
 import Control.Monad.Logger
 import Control.Monad.Trans.Control
 import Dismissive.Api
+import Dismissive.Mailer
 import Servant
 
-type Dismiss = DismissiveT (EitherT ServantErr IO) :~> EitherT ServantErr IO
+type Dismiss = DismissiveT (MailerT (EitherT ServantErr IO)) :~> EitherT ServantErr IO
 
 getDismiss :: (MonadIO m, MonadBaseControl IO m, MonadBaseControl IO m', MonadLogger m', MonadIO m')
            => ConnectionString
-           -> ((DismissiveT m :~> m) -> m' a)
+           -> MailerConf
+           -> ((DismissiveT (MailerT m) :~> m) -> m' a)
            -> m' a
-getDismiss connStr f =
-  withDismissiveIO
-  connStr
-  -- GHC can't typecheck the pointfree equivalent (f . Nat), and I don't know why
-  (\r -> f $ Nat r)
+getDismiss connStr mailerConf f =
+  withDismissiveIO connStr $ \runDismissiveT ->
+    f $ Nat (runMailerT mailerConf . runDismissiveT)
